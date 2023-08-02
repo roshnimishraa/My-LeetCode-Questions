367497c9453555d1f4fd587e78fc60c1323f0a8c
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-02 12:00:19 +0530

    Update and rename Permutations.cpp to Day 2 Permutations.cpp

--- Leetcode Daily Challenge/Day 2 Permutations.cpp
+++ Leetcode Daily Challenge/Day 2 Permutations.cpp
@@ -42,4 +42,38 @@ public:
         return ans;
     }
 };
+------------------------------------------------------------------------------------------------------------------------------------
 
+Approach 2 : doing swaping between elements of an array
+1. index = i (to avoid duplicacy) 
+2. Corner Case ->index >= n from element in ans and return 
+3. Do swap (nums[i],nums[index])
+4. Do recursive call from index+1
+5. Undo swap
+
+class Solution {
+    void helper(vector<int> &nums,int n,int index,vector<vector<int>> &ans)
+    {
+        // corner case  
+        //index out of bound
+        if(index>=n){
+      ans.push_back(nums);
+            return;
+}
+        for(int i=index;i<n;i++){
+//swap
+            swap(nums[i],nums[index]);
+            helper(nums,n,index+1,ans);
+            swap(nums[i],nums[index]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+
+        int index=0;
+        helper(nums,n,index,ans);
+        return ans;
+    }
+};
