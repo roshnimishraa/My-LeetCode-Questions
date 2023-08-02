00e8ed5328e274697de05e478dc491c915c87228
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-02 16:00:56 +0530

    Update Nearest Greater to Right OR Next Largest Element.cpp

--- Stack and Queues/Easy/Nearest Greater to Right OR Next Largest Element.cpp
+++ Stack and Queues/Easy/Nearest Greater to Right OR Next Largest Element.cpp
@@ -1,1 +1,35 @@
+Approach : stack & nextGr array
+  1. pushing last element into st and initializing nextGr as -1
+  2. checking array elements from n-2 if 
+a) st.size() > 0 && st.top() <= arr[i] -> Agar stack ka top chhota arr[i] then use stack se pop kro 
+b) if st.empty() == true -> nextGr = -1
+  else nextGr[i] = st.top()
+3. Push all the elements of array in stack at the greater element to right is left 
 
+ Time Complexity : O(N)
+ Auxiliary Space : O(N)
+
+  class Solution
+{
+    public:
+    vector<long long> nextLargerElement(vector<long long> arr, int n){
+     stack<long long> st;
+    st.push(arr[n-1]);
+    vector<long long> nextGr(n);
+ nextGr[n-1] = -1;
+     for(int i=n-2;i>=0;i--){     
+         while(st.size() > 0 && st.top() <= arr[i]){
+             st.pop();
+         }     
+  if(st.size() == 0){
+      nextGr[i] = -1;
+  }
+  else {
+      nextGr[i] = st.top();
+  }
+  st.push(arr[i]);
+     }
+   
+     return nextGr;
+    }
+};
