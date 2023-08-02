fd773e1f751a45f3170356aeb71d4a68ac8ffbce
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-02 12:37:30 +0530

    Update Day 1 Combinations.cpp

--- Leetcode Daily Challenge/Day 1 Combinations.cpp
+++ Leetcode Daily Challenge/Day 1 Combinations.cpp
@@ -38,3 +38,29 @@ public:
         return ans;
     }
 };
+----------------------------------------------------------------------------------------------------------------------------------
+
+Approach 2 : using For loop
+
+class Solution {
+    void helper(int index,int n,int k,vector<int> &output,vector<vector<int>> &ans)
+    {
+        if(k==0){
+            ans.push_back(output);
+            return;
+        }
+        for(int i=index;i<=n;i++){
+            output.push_back(i);
+            helper(i+1,n,k-1,output,ans);
+            output.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index =1;
+        helper(index,n,k,output,ans);
+        return ans;
+    }
+};
