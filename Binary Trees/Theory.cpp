7de88eefd172157fa6cad5f5a7a4aead20709dfd
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-16 14:16:06 +0530

    Create Theory.cpp

---
+++ Binary Trees/Theory.cpp
@@ -0,0 +1,25 @@
+Types of Binary Trees
+1. Complete Binary Tree
+2. Perfect Binary Tree
+3. Balanced Binary Tree 
+4. Degenerate Binary Tree
+5. Full Binary Tree 
+
+
+1. Complete Binary Tree : 
+  a) All levels are completely filled except the last level.
+  b) the last level has full nodes m as left as possible.
+
+2. Perfect Binary Tree : 
+   All the leaf nodes are at same level.
+
+3. Balanced Binary Tree : 
+   Height of tree at max log(N) nodes
+ n =8 
+log 8 = 3 (height should be 3)
+
+4. Degenerate Tree : Left skew or Right skew Tree
+
+5. Full Binary Tree : Either has 0 or 2 children. 
+
+  No. of Nodes in Binary Tree = 2 * h – 1
