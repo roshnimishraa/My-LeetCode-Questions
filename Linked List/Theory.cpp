973b47d9cdb460c92750bde483181f37897c75be
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-16 16:22:38 +0530

    Create Theory.cpp

---
+++ Linked List/Theory.cpp
@@ -0,0 +1,11 @@
+1. Non Contiguous memory location
+2. No pre-allocation of memory
+
+Application of Linked List 
+1. Worst Case Insertions(at End and Beginning) take O(1) time.
+2. Worst Case deletions(from the Beginning) take O(1) time.
+3. Insertions and Deletions in the middle are Theta(1) if we have reference to the previous node.
+4. Round Robin Implementation
+5. Merging two sorted Linked Lists is faster than Arrays
+6. Implementation of simple memory manager where is need to link free blocks is easier
+7. Easy implementation of Queue and Dequeue Data Structures.
