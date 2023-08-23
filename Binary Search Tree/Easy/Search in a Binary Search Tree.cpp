7e1750f0888a9e37252f7a3a688cc936fbfbc2da
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-23 13:32:58 +0530

    Create Search in a Binary Search Tree.cpp

---
+++ Binary Search Tree/Easy/Search in a Binary Search Tree.cpp
@@ -0,0 +1,17 @@
+Time Complexity : O(H)
+    Space Complexity : O(H)
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root != NULL && root->val != val){
+            if(val < root->val){
+                root = root->left;
+            }
+            else{
+                root = root->right;
+            }
+        }
+        return root;
+    }
+};
