b0c8f3014425a7a68f9cb04aeaf7d88b85900ac1
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-25 15:12:12 +0530

    Create Theory.cpp

---
+++ Graphs/Theory.cpp
@@ -0,0 +1,12 @@
+Path : 
+i) The path contains a lot of nodes and each of them is reachable.
+ii) A node can not appear twice in a path 
+
+Degree of Graph:
+It is the number of edges that go inside or outside that node.
+
+  For Undirected Graph : Total Degree of a graph = 2 * Edges 
+eg.  social network
+
+For Directed Graph : The indegree of a node is the number of incoming edges. The outdegree of a node is the number of outgoing edges.
+eg. www
