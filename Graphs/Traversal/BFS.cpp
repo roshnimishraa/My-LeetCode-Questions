0758cef4098a8e9f32c6f8d1a28025801dcd5215
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-26 00:06:13 +0530

    Update BFS.cpp

--- Graphs/Traversal/BFS.cpp
+++ Graphs/Traversal/BFS.cpp
@@ -13,3 +13,36 @@ Approach :
 3. All the unvisited adjacent nodes from ‘v’ are visited next and are pushed into the queue. 
   The list of adjacent neighbors of the node can be accessed from the adjacency list.
 4. Repeat steps 2 and 3 until the queue becomes empty, and this way you can easily traverse all the nodes in the graph.
+
+
+  
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+  
+//taking a visited array and Initially marked as 0
+int vis[V]={0};
+//In BFS,start with a “starting” node, mark it as visited,
+//and push it into the queue data structure.
+  vis[0] = 1;
+
+  queue<int> q;
+  q.push(0);  //push starting node
+  
+ // store bfs traversal 
+ vector<int> bfs;
+  while(!q.empty()){
+      int node = q.front();
+      q.pop();
+      
+     bfs.push_back(node);
+     
+     for(auto it : adj[node]){
+         //check if previously visited that node
+         if(!vis[it]){
+             vis[it]=1;
+             q.push(it);
+         }
+     }
+  }
+  return bfs;
+    }
+};
