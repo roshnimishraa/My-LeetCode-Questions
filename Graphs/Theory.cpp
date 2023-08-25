64312a121074b9ea50730a5d2f778afe2148a6b2
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-25 18:54:37 +0530

    Update Theory.cpp

--- Graphs/Theory.cpp
+++ Graphs/Theory.cpp
@@ -2,6 +2,10 @@ Path :
 i) The path contains a lot of nodes and each of them is reachable.
 ii) A node can not appear twice in a path 
 
+Walk : 
+i) The path contains a lot of nodes and each of them is reachable.
+ii) A node can appear twice in a path 
+
 Degree of Graph:
 It is the number of edges that go inside or outside that node.
 
@@ -10,3 +14,10 @@ eg.  social network
 
 For Directed Graph : The indegree of a node is the number of incoming edges. The outdegree of a node is the number of outgoing edges.
 eg. www(world wide web)
+
+Graph Representation 
+ 1. Adjacency Matrix : It is symmetric for undirected graph
+     Space Required : O(V * V) 
+ 2. Adjacency List
+     Space Required : O(2 * Edges) 
+  
