ed3f9a08502caf4a2320c9418ca88d639f193f19
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-26 16:29:15 +0530

    Update Find number of connected components or Number of Provinces.cpp

--- Graphs/Medium/Find number of connected components or Number of Provinces.cpp
+++ Graphs/Medium/Find number of connected components or Number of Provinces.cpp
@@ -1,41 +1,39 @@
 TC : O(N) + O(V + 2 * E)
 sc : O(N)
 
-void dfs(int node,vector<int> adjLs[],int vis[])
-{
-    vis[node]=1;
-  //  adjLs.push_back(node);
-
-    for(auto it: adjLs[node])
+class Solution {
+    private:
+    void dfs(int node,vector<int> &vis,vector<int> adj[])
     {
-        if(vis[it]==0){
-            dfs(it,adjLs,vis);
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(vis[it] == 0){
+                dfs(it,vis,adj);
+            }
         }
     }
-}
-
-int findNumOfProvinces(vector<vector<int>>& roads, int n) {
-vector<int> adjLs[n];
-// make adjacency list
- for(int i=0;i<n;i++){
-     for(int j=0;j<n;j++)
-     {
-    // ifadj[i][j]== 1 mean there is a node i!=j and it should not 
-// self node
-         if(roads[i][j] == 1 && i !=j){
-             adjLs[i].push_back(j);
-             adjLs[j].push_back(i);
-         }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> adjLs[n];
+vector<int> vis(n,0);
+       for(int i =0;i<n;i++)
+       {
+           for(int j=0;j<n;j++){
+        if(isConnected[i][j]==1 && i != j){
+            adjLs[i].push_back(j);
+            adjLs[j].push_back(i);
+        }
+           }
+       }
+ int count=0;
+ for(int i=0;i<n;i++)
+ {
+     if(!vis[i]){
+         count++;
+         dfs(i,vis,adjLs);
      }
  }
-
- int vis[n] = {0};
- int count=0;
-for(int i= 0;i<n;i++){
-if(vis[i] == 0){
-    count++;
-    dfs(i,adjLs,vis);
-}
-}
-return count;
-}
+ return count;
+    }
+};
