083231dca90f2e84720a13b4dbb346dcdb2c169c
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-27 14:13:27 +0530

    Update Detect Cycle in an Undirected Graph.cpp

--- Graphs/Medium/Detect Cycle in an Undirected Graph.cpp
+++ Graphs/Medium/Detect Cycle in an Undirected Graph.cpp
@@ -48,3 +48,47 @@ for(int i=0;i<V;i++){
 return false;
     }
 };
+
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  Using DFS (Depth First Search) 
+TC : o(N + 2*E) 
+sc : O(N) 
+
+class Graph {
+private : 
+ bool detect(int node,int parent,int vis[],vector<int> adj[])
+ {
+     vis[node] = 1;
+     
+    for(auto adjacentNode : adj[node]){
+    if(!vis[adjacentNode])
+   {
+         if(detect(adjacentNode,node,vis,adj)==true)
+  return true;
+  
+   }
+   else if(adjacentNode != parent){
+       return true;
+   } 
+  }
+   
+    return false;
+ }
+public:
+    bool detectCycle(int V, vector<int> adj[]) {
+       int vis[V]={0};
+    //for multiple graph
+for(int i=0;i<V;i++)
+{
+    if(!vis[i]){
+        if(detect(i,-1,vis,adj)==true)
+        return true;
+        
+    }
+}
+      return false;
+    }
+};
