6823c13cdcfd0b184751d838e13617887901c4b2
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-08-29 23:39:37 +0530

    Update Delete Node in a BST.cpp

--- Binary Search Tree/Medium/Delete Node in a BST.cpp
+++ Binary Search Tree/Medium/Delete Node in a BST.cpp
@@ -2,60 +2,67 @@
 Time Complexity: O(h), where h is the height of the BST. 
 Auxiliary Space: O(n).
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-public:
-    int MaxLeft(TreeNode* root)
+    private : 
+    int maxLeft(TreeNode* root)
     {
-while(root->right!=nullptr)
-        {
-            root=root->right;
 
+        while(root != NULL && root->right != NULL){
+            root = root->right;
         }
         return root->val;
     }
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root == NULL){
+       if(root == NULL){
             return root;
         }
-        if(root->val == key){
-            // 0 Child
-            if(root->left == NULL && root->right == NULL){
-              delete root;
-              return NULL;
-            }
-// 1 Child
+
+if(root->val == key)
+    {
+// 1. leaf node
+if(root->left == NULL && root->right == NULL){
+    delete root;
+    return NULL;
+}
+
+// 1 child 
 if(root->left != NULL && root->right == NULL)
 {
-  TreeNode* temp= root->left;
-
-  return temp;
+TreeNode *temp = root->left;
+ return temp;
 }
 if(root->left == NULL && root->right != NULL){
-    TreeNode* temp = root->right;
-
-    return temp ;
+TreeNode *temp = root->right;
+ return temp;
 }
-// 2 Child
+
+// 2 child
 if(root->left != NULL && root->right != NULL){
-    int leftmax= MaxLeft(root->left);
- root->val=leftmax;
-   root->left=deleteNode(root->left,leftmax);
-    return root;
+int maxi = maxLeft(root->left);
+root->val = maxi;
+root->left = deleteNode(root->left,maxi);
+return root;
 }
-}
-else if(root->val >key){
-    root->left = deleteNode(root->left,key);
+    }
 
+// search function 
+else if(root->val > key){
+root->left = deleteNode(root->left,key);
 }
 else if(root->val < key){
-    root->right = deleteNode(root->right,key);
-
-}
-
-
-else{
-    root->right = deleteNode(root->right,key);
+ root->right = deleteNode(root->right,key);
 }
 
 return root;
