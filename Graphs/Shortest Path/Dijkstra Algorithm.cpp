8f3b6f95259eda3f16e7b0854b6d198299185cce
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-01 14:43:52 +0530

    Create Dijkstra Algorithm.cpp

---
+++ Graphs/Shortest Path/Dijkstra Algorithm.cpp
@@ -0,0 +1,13 @@
+Dijkstra Algorithm ~ Single Source Shortest Path 
+Very Famous Greedy Algorithm and give Optimal solution 
+
+>> Condition : 
+1. Works only for Connected Graph
+2. It works only for those graph that don't contain any negative weight edge.
+3. It only provides the value or cost of the shortest path 
+4. Works for both directed and undirected graph
+
+It can be implemented using : 
+1. Queue : More Slow 
+2. Priority Queue : Slow 
+3. Set : Fast (TC)
