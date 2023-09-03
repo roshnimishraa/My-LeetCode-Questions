3e63d9f9be49b82b9d6439eaa53405d6a9553203
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-03 14:21:24 +0530

    Create Next Permutation.cpp

---
+++ Arrays/Medium/Next Permutation.cpp
@@ -0,0 +1,8 @@
+Approach 1 : Using Built-in function next_permutation 
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+    }
+};
