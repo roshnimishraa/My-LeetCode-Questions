a385f8df19f395976a23b998cf1209d954311333
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-17 15:53:49 +0530

    Update Topological sort using DFS.cpp

--- Graphs/Topological Sort/Topological sort using DFS.cpp
+++ Graphs/Topological Sort/Topological sort using DFS.cpp
@@ -3,47 +3,59 @@ Time Complexity: O(V+E)+O(V), where V = no. of nodes and E = no. of edges. There
 Space Complexity: O(2N) + O(N) ~ O(2N): O(2N) for the visited array and the stack 
 carried during DFS calls and O(N) for recursive stack space, where N = no. of nodes.
 
-1. use stack and vis array
-2. traverse through all adj nodes and push into st
-3. pop element from st and store in vector (topo sort) 
-4. return topo sort 
+Approach : 
+1. use a stack and a visited array
+2. explore all the neighbours of node and push into stack and mark 1 in visited array 
+3. if stack is empty pop of out and push into ans vector 
 
-class Solution
+why DFS Traversal ?
+	DFS goes in-depth, i.e., traverses all nodes by going ahead, and when there are no further nodes to traverse in the current path,
+then it backtracks on the same path and traverses other unvisited nodes.
+
+
+Eg. For the edges connection 
+5->0
+4->0
+5->2
+2->3
+3->1
+4->1
+
+Topological sort is : 5  4  3  2  1  0 or 4  5  2  3  1  0
+
+  Implementation 
+  
+  class Solution
 {
     private:
-    void dfs(int node,int vis[],stack<int> &st,vector<int> adj[])
+    void dfs(int node,int vis[],vector<int> adj[],stack<int> &st)
     {
         vis[node]=1;
-        
-    for(auto it : adj[node])
-    {
-        if(!vis[it] )
-        {
-            dfs(it,vis,st,adj);
+        for(auto it : adj[node]){
+            if(!vis[it])
+            dfs(it,vis,adj,st);
         }
-    }
-    st.push(node);
+        st.push(node);
     }
 	public:
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    int vis[V]={0};
-	 stack<int> st;
-	 for(int i=0;i<V;i++)
-	 {
-	     if(!vis[i])
-	     {
-	         dfs(i,vis,st,adj);
-	     }
-	 }
-	 vector<int> ans;
-	 while(!st.empty())
-	 {
-	     ans.push_back(st.top());
-	     st.pop();
-	     
-	 }
-	 return ans;
+	    int vis[V] = {0};
+	    stack<int> st;
+	    for(int i=0;i<V;i++)
+	    {
+	        if(!vis[i]){
+	            dfs(i,vis,adj,st);
+	        }
+	    }
+	    //push in st
+	    vector<int> ans;
+	    while(!st.empty())
+	    {
+	        ans.push_back(st.top());
+	        st.pop();
+	    }
+	    return ans;
 	}
 };
