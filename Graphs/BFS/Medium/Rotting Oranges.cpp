1fe3127a017872b4a8ddfad3ccacf1df3cb02a2f
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-17 17:54:37 +0530

    Update Rotting Oranges.cpp

--- Graphs/BFS/Medium/Rotting Oranges.cpp
+++ Graphs/BFS/Medium/Rotting Oranges.cpp
@@ -73,3 +73,69 @@ if(count != cntfresh) return -1;
 return time;
     }
 };
+
+-------------------------------------------------------------------------------------------------------------------------------
+ Another way 
+ class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+    
+// in q store {{row,col},time}
+queue<pair<pair<int,int>,int>> q;
+int vis[n][m];
+
+// push rotten oranges in q with time =0 
+for(int i=0;i<n;i++)
+{
+    for(int j=0;j<m;j++)
+    {
+        if(grid[i][j]==2){
+            q.push({{i,j},0});
+            vis[i][j]=2;
+        }
+        else {
+            vis[i][j]=0;
+        }
+    } 
+}
+
+// compute time 
+int time =0;
+int dx[]={-1,0,1,0};
+int dy[]={0,1,0,-1};
+while(!q.empty()){
+    int row = q.front().first.first;
+    int col = q.front().first.second;
+    int tm = q.front().second;
+    time = max(time,tm);
+    q.pop();
+
+    //visit neighbour nodes in 4 directions 
+for(int i=0;i<4;i++)
+{
+    int nrow=row+dx[i];
+    int ncol = col+dy[i];
+
+if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol] ==0 && grid[nrow][ncol]==1)
+{
+ q.push({{nrow,ncol},tm+1});
+ vis[nrow][ncol]=2;
+
+}
+}
+}
+
+for(int i=0;i<n;i++)
+{
+    for(int j=0;j<m;j++)
+    {
+        if(vis[i][j] != 2 && grid[i][j]==1)
+        return -1;
+    }
+}
+
+return time;
+    }
+};
