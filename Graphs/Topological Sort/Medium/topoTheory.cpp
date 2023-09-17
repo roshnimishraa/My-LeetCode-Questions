8b96bad8b7ee53dece5558249022ad5403d96ce2
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-17 14:48:55 +0530

    Create topoTheory.cpp

---
+++ Graphs/Topological Sort/Medium/topoTheory.cpp
@@ -0,0 +1,4 @@
+Topological Sorting : 
+i) Linear ordering of vertices such that if there is an edge between u and v, u appears before v in that ordering 
+ii) Only exists in Directed Acyclic Graph (DAG) 
+
