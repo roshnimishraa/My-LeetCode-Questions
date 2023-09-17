07f6c6392b8e5a80d3b88b97988b0e455485a0c0
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-17 18:07:09 +0530

    Update Summary.cpp

--- Graphs/Summary.cpp
+++ Graphs/Summary.cpp
@@ -1,18 +1,22 @@
 1. Time Complexity of DFS & BFS -> O(V) + O(E) 
+ BFS : Queue and vis 
+ DFS : Recursion and vis 
 
-2. Cycle Detection in Undirected Graph
+2. Rotten Oranges : Return min time so traverse level wise (BFS) 
+
+3. Cycle Detection in Undirected Graph
 a) DFS -> parent & node & vis
 b) BFS -> queue {node,parent}, vis
 
-3. Detect Cycle in Directed Graph 
+4. Detect Cycle in Directed Graph 
 a) DFS -> vis & pathVis
 b) BFS -> 
 
-4. DFS Approach (Islands) 
+5. DFS Approach (Islands) 
 i) No. of Islands -> DFS traversal & vis & count 
 ii) No. of Distinct Island -> DFS & vis & vector of pair of list & set to store unique list return st.size() 
   
-5. Bipartite Graph 
+6. Bipartite Graph 
  Time complexity: O(V+E).
 Auxiliary Space: O(V)
 i) BFS Approach : 
@@ -20,9 +24,9 @@ Take queue and color(v,-1) take color initlially 0
 ii) DFS Approach : 
 Take currentColor,color,node,adj
 
-6. Topological Sort 
+7. Topological Sort 
 a) DFS : use stack and vis array and ans vec to store topo sort  
 TC : O(V+E)+O(V)
-b) BFS(Kahn's Algorithm) 
+b) BFS(Kahn's Algorithm) : use indegree array,queue 
 
   
