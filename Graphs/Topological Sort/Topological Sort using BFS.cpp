2696b8a178ca7d4e39723a77578fac5c7f4abcf3
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-17 16:21:48 +0530

    Update Topological Sort using BFS.cpp

--- Graphs/Topological Sort/Topological Sort using BFS.cpp
+++ Graphs/Topological Sort/Topological Sort using BFS.cpp
@@ -1,9 +1,14 @@
-Kahn's Algorithm ~ Topological sort using BFS
+Approach : Kahn's Algorithm ~ Topological sort using BFS
 
 Time Complexity: O(V+E), where V = no. of nodes and E = no. of edges. This is a simple BFS algorithm
-Space Complexity: O(N) + O(N) ~ O(2N), O(N) for the indegree array, and O(N) for the queue data structure used in BFS(where N = no.of nodes)
+Space Complexity: O(N) + O(N) ~ O(2N), O(N) for the indegree array, and O(N) for the queue data structure used 
+in BFS(where N = no.of nodes)
 
-/*   Implementation  */
+Algorithm : 
+1. take a array of indegree intially assigned 0 
+2. now put indegree of nodes in array 
+3. take a queue insert node with indegree 0 into the queue and explore its adjacent nodes
+4. while taking nodes from indegree array and pushing into q decrease indegree of node.
 
 class Solution
 {
@@ -11,44 +16,38 @@ class Solution
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	 int indegree[V] ={0}; //initially indegree of nodes is 0 
-	 
-	//calculate indegree 
-	for(int i=0;i<V;i++)
-	{
-	   for(auto it : adj[i]){
+	    int indegree[V]={0};
+	   
+	  for(int i=0;i<V;i++)
+	  {
+	   for(auto it : adj[i])
+	   {
 	       indegree[it]++;
 	   }
-	}
+	  }
+	
 	queue<int> q;
-	for(int i=0;i<V;i++){
-	   if(indegree[i] == 0) {
-	       q.push(i);
-	   }
+	for(int i=0;i<V;i++)
+	{
+	    if(indegree[i]==0){
+	        q.push(i);
+	    }
 	}
-	//store ans in vector 
 	vector<int> topo;
-	//push 0 indegree node in queue
 	while(!q.empty())
 	{
-	    int node =q.front();
-	   q.pop();
-	   
-	  topo.push_back(node);
-	  
-	 // traverse all its adjacent nodes 
-	for(auto it : adj[node])
-	{
-	   // node is in used or store in vector ans 
-	   // therefore decrease indegree
-	   indegree[it]--;
-	   
-	  //if after decreasing indegree it becomes 0 then push 
-	  // into queue
-	 if(indegree[it] == 0)
-	  q.push(it);
-	}
-	   
+	    int node = q.front();
+	    q.pop();
+	    topo.push_back(node);
+	    
+	   for(auto it : adj[node])
+	   {
+	       indegree[it]--;
+	       
+	   if(indegree[it]==0){
+	       q.push(it);
+	   }
+	   }
 	}
 	return topo;
 	}
