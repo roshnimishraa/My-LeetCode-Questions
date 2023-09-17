77d64fd631583c018c4ecd4fbd547a02798ea5a4
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-17 15:45:32 +0530

    Update Topological sort using DFS.cpp

--- Graphs/Topological Sort/Topological sort using DFS.cpp
+++ Graphs/Topological Sort/Topological sort using DFS.cpp
@@ -1,3 +1,13 @@
+Time Complexity: O(V+E)+O(V), where V = no. of nodes and E = no. of edges. There can be at most V components. 
+  So, another O(V) time complexity.
+Space Complexity: O(2N) + O(N) ~ O(2N): O(2N) for the visited array and the stack 
+carried during DFS calls and O(N) for recursive stack space, where N = no. of nodes.
+
+1. use stack and vis array
+2. traverse through all adj nodes and push into st
+3. pop element from st and store in vector (topo sort) 
+4. return topo sort 
+
 class Solution
 {
     private:
