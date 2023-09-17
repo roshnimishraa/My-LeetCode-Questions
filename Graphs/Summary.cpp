27c98f0b1328c9a21011f8300d9721e74bd7cbc4
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-17 14:39:59 +0530

    Create Summary.cpp

---
+++ Graphs/Summary.cpp
@@ -0,0 +1,24 @@
+1. Time Complexity of DFS & BFS -> O(V) + O(E) 
+
+2. Cycle Detection in Undirected Graph
+a) DFS -> parent & node & vis
+b) BFS -> queue {node,parent}, vis
+
+3. Detect Cycle in Directed Graph 
+a) DFS -> vis & pathVis
+b) BFS -> 
+
+4. DFS Approach (Islands) 
+i) No. of Islands -> DFS traversal & vis & count 
+ii) No. of Distinct Island -> DFS & vis & vector of pair of list & set to store unique list return st.size() 
+  
+5. Bipartite Graph 
+ Time complexity: O(V+E).
+Auxiliary Space: O(V)
+  
+i) BFS Approach : 
+Take queue and color(v,-1) take color initlially 0 
+ii) DFS Approach : 
+Take currentColor,color,node,adj
+
+  
