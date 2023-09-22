f62961b6d422aae89055f5e8c2f98b32eac60ecd
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-22 10:51:56 +0530

    Update Distance of nearest cell having 1 OR 01 Matrix.cpp

--- Graphs/BFS/Medium/Distance of nearest cell having 1 OR 01 Matrix.cpp
+++ Graphs/BFS/Medium/Distance of nearest cell having 1 OR 01 Matrix.cpp
@@ -1,56 +1,64 @@
 TC : O(N * M * 4) 
 
-class Solution 
+ class Solution 
 {
     public:
     //Function to find distance of nearest 1 in the grid for each cell.
 	vector<vector<int>>nearest(vector<vector<int>>grid)
 	{
-         int n = grid.size();
-         int m = grid[0].size();
-         
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<vector<int>> dist(n,vector<int>(m,0));
-        
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            //consider 1
-        if(grid[i][j] == 1) 
+      int n = grid.size();
+      int m = grid[0].size();
+    //   {{row,col},dis}
+     queue<pair<pair<int,int>,int>> q;
+     vector<vector<int>> vis(n,vector<int> (m,0));
+     vector<vector<int>> dist(n,vector<int>(m,0));
+     
+     // mark vis of grid[i][j]=1
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
         {
-             vis[i][j] = 1;
-        q.push({{i,j},0});
-        }
-  else{
+            if(grid[i][j]==1)
+            {
+                vis[i][j]=1;
+                q.push({{i,j},0});
+            }
+              else{
              vis[i][j] = 0;
   }
  
         }
-        
     }
-    int delrow[] = {-1,0,1,0};
-    int delcol[] = {0,1,0,-1};
-    while(!q.empty()){
-        int row = q.front().first.first;
-        int col = q.front().first.second;
-        int steps = q.front().second;
-        q.pop();
-        
-    // calculate distance 
-    dist[row][col] = steps;
-        for(int i=0;i<4;i++){
-        // explore neighbours 
-    int nrow = row + delrow[i];
-    int ncol = col + delcol[i];
+    // explore adj nodes in 4 possible directions 
+    int dx[]={-1,0,1,0};
+    int dy[]={0,1,0,-1};
     
-//check valid 
-if(nrow>=0 && nrow < n && ncol>=0 && ncol<m 
-&& vis[nrow][ncol] == 0){
-    vis[nrow][ncol] = 1;
+    while(!q.empty())
+    {
+        auto it = q.front();
+        q.pop();
+        int row=it.first.first;
+        int col = it.first.second;
+        int steps = it.second;
+
+	//Calculate distance
+    dist[row][col]=steps;
+    
+for(int i=0;i<4;i++)
+{
+    int nrow= row + dx[i];
+    int ncol = col + dy[i];
+
+// valid check 
+if(nrow>=0 && nrow<n && ncol>=0 && ncol <m && 
+vis[nrow][ncol]==0)
+{
+    vis[nrow][ncol]=1;
     q.push({{nrow,ncol},steps+1});
 }
-        }
+}
     }
-    return dist;
+    
+ return dist;     
 	}
 };
