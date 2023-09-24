95c0bcfbc741693c658e0d8138e6a8239c5190aa
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-09-24 17:03:31 +0530

    Update Kernighan's Algorithm OR Count Set Bits in an Integer.cpp

--- Bit Magic/Kernighan's Algorithm OR Count Set Bits in an Integer.cpp
+++ Bit Magic/Kernighan's Algorithm OR Count Set Bits in an Integer.cpp
@@ -1,1 +1,16 @@
+Appraoch:
+1. find right most set bit mask 
+2. after cal rsbm mark as 0 and increase count 
+     repeat this step until n!=0 
 
+int countSetBits(int N)
+{
+    int count=0;
+    while(N!=0)
+    {
+        int rsbm = N & -N;
+        N-= rsbm;
+        count++;
+    }
+return count;
+}
