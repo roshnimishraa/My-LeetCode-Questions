9e075e8e2a8c11e09ec8d85cad2b9feb9595c79b
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-10-02 18:04:23 +0530

    Create Remove Colored Pieces if Both Neighbors are the Same Color.cpp

---
+++ Greedy/Medium/Remove Colored Pieces if Both Neighbors are the Same Color.cpp
@@ -0,0 +1,26 @@
+Problem Link : https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/
+
+TC : O(N) 
+SC : O(1) 
+  
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int n =colors.length();
+        int alice=0;
+        int bob=0;
+    for(int i=1;i<n-1;i++)
+    {
+        if(colors[i-1] == colors[i] && colors[i] == colors[i+1])
+        {
+            if(colors[i] == 'A')
+               alice++; //alice will delete
+            
+            else
+                bob++;
+            
+        }
+    }
+     return alice > bob;
+    }
+};
