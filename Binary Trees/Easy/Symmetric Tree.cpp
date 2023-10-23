b1a8ea2fd60e2f1243775575e8d7ee00b14ea735
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-10-23 20:47:27 +0530

    Update Symmetric Tree.cpp

--- Binary Trees/Easy/Symmetric Tree.cpp
+++ Binary Trees/Easy/Symmetric Tree.cpp
@@ -7,21 +7,37 @@ Reason: We are doing simple tree traversal and changing both root1 and root2 sim
 Space Complexity: O(N)
 Reason: In the worst case (skewed tree), space complexity can be O(N).
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-    bool helper(TreeNode* left,TreeNode* right)
-    {
-
-    if(left == NULL || right == NULL) return left == right;
-        
-    if(left->val != right->val) return false;
-        
-    return helper(left->left,right->right) && helper(left->right,right->left);
-    }
 public:
+bool helper(TreeNode* left,TreeNode* right)
+{
+
+if(left == NULL && right == NULL) {
+    return true;
+}
+if(left == NULL || right == NULL){
+    return false;
+}
+if(left->val != right -> val){
+    return false;
+}    
+    
+//comparing left subtree's left child with right subtree's right child --//AND-- comparing left subtree's right child with right subtree's
+//left child
+return helper(left->left,right->right) && helper(left->right,right->left);
+}
     bool isSymmetric(TreeNode* root) {
-        if(root == NULL || helper(root->left,root->right) )
-            return true;
-        else 
-            return false;
+    return (root == NULL || helper(root->left,root->right));
     }
 };
