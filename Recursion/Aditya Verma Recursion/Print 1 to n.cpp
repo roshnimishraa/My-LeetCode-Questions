9364b68d92e7b8f1d420c783589b63cf84cd34d5
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-15 00:03:32 +0530

    Update Print 1 to n.cpp

--- Recursion/Aditya Verma Recursion/Print 1 to n.cpp
+++ Recursion/Aditya Verma Recursion/Print 1 to n.cpp
@@ -1,5 +1,6 @@
 Hypothesis - Induction - Base Method 
 
+Print 1 to N 
 void fun(int n) {
   if(n == 1){
 cout<<n<<" ";
@@ -7,4 +8,17 @@ cout<<n<<" ";
   }
 fun(n-1);
 cout<<n<<" ";
+} 
+
+
+Print N to 1 
+
+void fun(int n) {
+  if(n == 1){
+cout<<n<<" ";
+    return;
+  }
+cout<<n<<" ";  
+fun(n-1);
+
 } 
