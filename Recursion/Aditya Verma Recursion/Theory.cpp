2c7d3fd6bc7a25991bbaf1e21f068d3acec127cd
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-14 23:57:34 +0530

    Create Theory.cpp

---
+++ Recursion/Aditya Verma Recursion/Theory.cpp
@@ -0,0 +1,31 @@
+>> When to use Recursion 
+  Decision -> as a result -> Input smaller 
+
+>> Recursive Tree -> Input - output method 
+
+>> How to solve Recursion problem 
+1. Design a Recursive tree
+2. Write the code
+
+>> Ways to solve Recursive problem 
+For Simple problem -> use Induction - Base - Hypothesis method 
+For Medium problem -> Recursive Tree 
+For Hard problem -> choice diagram 
+
+Recursion Problem 
+>>> I/P - O/P method 
+1. Print 1 to N/ N to ` 
+2. Sort an array
+3. Delete middle element in a stack 
+4. Remove duplicates from a string 
+5. Count the number of occurences 
+6. Subset 
+7. Permutation (a) with space (variations) 
+               (b) with case changes
+8. Josephus problem 
+
+>>> Extended I/P - O/P method 
+1. Binary string with no. of 1's > No. of 0's 
+2. Generate Balanced Parenthesis 
+
+
