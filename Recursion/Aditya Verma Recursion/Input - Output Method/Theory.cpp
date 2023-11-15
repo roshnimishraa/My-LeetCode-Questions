96d2ea6c6986754ed3028fbe0462ed8eea88322e
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-15 16:40:28 +0530

    Create Theory.cpp

---
+++ Recursion/Aditya Verma Recursion/Input - Output Method/Theory.cpp
@@ -0,0 +1,7 @@
+Problem based on I/P - O/P method 
+1. Subset 
+2. Subset duplicates + other variation
+3. Permutation with space
+4. Permutation with case change
+5. Josephus problem 
+6. Print space string 
