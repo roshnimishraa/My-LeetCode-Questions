cdf6fda97ec3090c95bf0fbccb480aac52ba93f9
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-15 15:26:19 +0530

    Create K-th Symbol in Grammar.cpp

---
+++ Recursion/Aditya Verma Recursion/K-th Symbol in Grammar.cpp
@@ -0,0 +1,39 @@
+Logic 
+for s1 : 0 
+    s2 : 0 1 
+    s3 : 0 1 1 0 
+    s4 : 0 1 1 0 1 0 0 1 
+    s5 : 0 1 1 0 1 0 0 1 1 0 0 1 0 1 1 0 
+
+Eg. n = 5, k = 4 
+  s5 kth character return 
+output : 1 
+
+Observation
+  length = 2^(n-1)
+if k<=mid  solve(n,k) -> solve(n-1,k) 
+else solve(n,k) -> !solve(n-1,k-mid) 
+
+
+class Solution {
+public:
+int solve(int n,int k){
+    //base case 
+    if(n == 1 && k == 1){
+        return 0;
+    }
+
+int length = pow(2,n-1);
+int mid = length/2;
+
+if(k <= mid){
+    return solve(n-1,k);
+}
+else {
+    return !solve(n-1,k-mid);
+}
+}
+    int kthGrammar(int n, int k) {
+        return solve(n,k);
+    }
+};
