efb81082749fbea0ea78ad2437a4820d38bd5f66
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-15 19:46:22 +0530

    Update Unique Subsets.cpp

--- Recursion/Aditya Verma Recursion/Input - Output Method/Unique Subsets.cpp
+++ Recursion/Aditya Verma Recursion/Input - Output Method/Unique Subsets.cpp
@@ -1,33 +1,38 @@
-class Solution
+/*
+Time Complexity  : O(2^N)
+Space Complexity : O(N)
+*/
+
+class Solution {
+public:
+void helper(int index,vector<int> &nums,vector<int> &output,vector<vector<int>> &ans)
 {
-    void helper(int index,vector<int> &arr,vector<int> &output,
-    vector<vector<int>> &ans)
-    {
-        if(index >= arr.size())
-        {
-            ans.push_back(output);
-            return;
-        }
-        
-        //include
-        output.push_back(arr[index]);
-helper(index+1,arr,output,ans);
+    if(index>= nums.size()){
+        ans.push_back(output);
+        return;
+    }
+    // take 
+output.push_back(nums[index]);
+helper(index+1,nums,output,ans);
 output.pop_back();
 
- while(index+1 <arr.size() && arr[index]==arr[index+1])
-    index++;
-helper(index+1,arr,output,ans);
-    }
-    public:
-    //Function to find all possible unique subsets.
-    vector<vector<int> > AllSubsets(vector<int> arr, int n)
-    {
-    vector<vector<int>> ans;
-    vector<int> output;
+// not take
+// skip consecutive element(duplicates)
+//eg. 1 2 2 3 
+while(index+1 < nums.size() && nums[index] == nums[index+1])
+{
+index++;
+}
+helper(index+1,nums,output,ans);
+
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+       vector<vector<int>> ans;
+       vector<int> output;
     int index=0;
-    sort(arr.begin(),arr.end());
-    helper(index,arr,output,ans);
+    sort(nums.begin(),nums.end());
+    helper(index,nums,output,ans);
     sort(ans.begin(),ans.end());
-    return ans;
+    return ans; 
     }
 };
