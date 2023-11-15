6366d8ae38dad85c0b74c2e3ea5f96d2a3bf45ef
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-15 16:38:37 +0530

    Create Theory.cpp

---
+++ Recursion/Aditya Verma Recursion/IBH Method/Theory.cpp
@@ -0,0 +1,11 @@
+Hypothesis - Base - Induction Method step
+
+I. Hypothesis method 
+1. Predict output function requirements 
+2. Same work decide for smaller input
+
+II. Induction method 
+do work 
+
+III. Base condition : Smallest valid input 
+
