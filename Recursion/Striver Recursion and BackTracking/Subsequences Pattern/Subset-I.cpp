7d298301bc6114d306f8bb54c64d16436af95230
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-11-17 19:48:46 +0530

    Update Subset-I.cpp

--- Recursion/Striver Recursion and BackTracking/Subsequences Pattern/Subset-I.cpp
+++ Recursion/Striver Recursion and BackTracking/Subsequences Pattern/Subset-I.cpp
@@ -1,27 +1,54 @@
 Naive Approach : Using Power Set 
 
 TC : O(N * 2^N) 
-SC : O(2^n) for storing subset sums, since 2^n subsets can be generated for an array of size n.
+SC : O(1) 
+    class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		vector<string> ans;
+		int n = s.length();
+		for(int num=0; num<2^n-1;num++){
+		   string str = ""; 
+		   for(int j = 0; j<n-1;j++){
+		       if(num & (1<<j)){
+		           str += s[j];
+		       }
+		   }
+		   		ans.push_back(str);
+		}
 
-void solve(vector<int>& nums,int index,int sum,vector<int> &ans)
+		return ans;
+		}
+};
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+Efficient Approach : Using Recursion 
+
+TC : O(2^N + 2^Nlog(2^N)) log n for sorting 
+SC : O(2^n) for storing subset sums, since 2^n subsets can be generated for an array of size n.
+class Solution
 {
-    if(index==nums.size()){
+public:
+void helper(int index,int sum,vector<int> &arr,vector<int> &ans)
+{
+    //base case when index out of bound then, ans is found 
+    if(index == arr.size()){
         ans.push_back(sum);
         return;
     }
-//take 
-solve(nums,index+1,sum+nums[index],ans);
-
-//not take 
-solve(nums,index+1,sum,ans);
+    // include
+    helper(index+1,sum+arr[index],arr,ans);
+    
+    // exclude
+    helper(index+1,sum,arr,ans);
 }
- 
-vector<int> subsetSum(vector<int> &num){
-
-      vector<int> ans;
-      int index =0;
-      int sum =0;
-  solve(num,index,sum,ans);
-    sort(ans.begin(),ans.end());
-    return ans; 
-}
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        int sum=0,index=0;
+        vector<int> ans;
+        helper(index,sum,arr,ans);
+    sort(arr.begin(),arr.end());
+    return ans;
+    }
+};
