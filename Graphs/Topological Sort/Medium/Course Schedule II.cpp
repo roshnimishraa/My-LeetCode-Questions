84b018801e9892fbb3de4b694ca4721d98fb7e49
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-02-15 23:53:32 +0530

    Update Course Schedule II.cpp

--- Graphs/Topological Sort/Medium/Course Schedule II.cpp
+++ Graphs/Topological Sort/Medium/Course Schedule II.cpp
@@ -1,67 +1,50 @@
 Same as Course Scheduler - I but 
 1. condition of true [0,1] course 0 should be taken before 1 and 
 2. return vector 
+
+       
 class Solution {
 public:
-    vector<int> findOrder(int V, vector<vector<int>>& prerequisites) {
-          
-        vector<int> adj[V];
-        
-        vector<int> indegree(V, 0);
-        
-        // make adjacency list
-        
-    for(auto it:prerequisites){
-        adj[it[1]].push_back(it[0]);
-
-        }
-
-
- for(int i=0;i<V;i++)
- {
-     for(auto it : adj[i]){
-      indegree[it]++;
-     }
- }
-        // push all the vertices with indegree == 0, into queue
-        
-        queue<int> q;
-        
-        for(int i = 0; i < V; i++)
-        {
-            if(indegree[i] == 0)
-            {
-                q.push(i);
-            }
-        }
-        
-      vector<int> topo;
-
-        while(!q.empty())
-        {
-            int u = q.front();
-            
-            q.pop();
-            
-            topo.push_back(u);
-            // for every adjacent of u, decrement the indegree
-            
-            for(auto v : adj[u])
-            {
-                indegree[v]--;
-                
-                // if indegree becomes 0, then push into queue
-                
-                if(indegree[v] == 0)
-                {
-                    q.push(v);
-                }
-            }
-        }
- //there is no cycle that's means no dependency       
-        if(topo.size() == V) 
-            return topo;
-        
-        return {};
+    vector<int> findOrder(int N, vector<vector<int>>& prerequisites)
+    {
+        int n = prerequisites.size();
+   vector<vector<int>> adj(N);
+for(auto it:prerequisites)
+{
+adj[it[1]].push_back(it[0]);
+}
+// kahn's algo
+vector<int> indegree(N,0);
+for(int i=0;i<N;i++)
+{
+for(auto it:adj[i])
+{
+    indegree[it]++;
+}
+}
+queue<int> q;
+for(int i=0;i<N;i++)
+{
+    if(indegree[i]==0)
+    {
+q.push(i);
+    }
+}
+vector<int> ans;
+while(!q.empty()){
+    auto node = q.front();
+    q.pop();
+ans.push_back(node);
+for(auto it:adj[node])
+{
+indegree[it]--;
+if(indegree[it] == 0)
+{
+    q.push(it);
+}
+}
+}
+if(ans.size() == N) return ans;
+else return {};
     }
 };
