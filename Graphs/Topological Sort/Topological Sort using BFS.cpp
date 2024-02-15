49c779390f2a6f0552dc49cb8bfae11435d7b4f7
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2024-02-15 21:43:34 +0530

    Update Topological Sort using BFS.cpp

--- Graphs/Topological Sort/Topological Sort using BFS.cpp
+++ Graphs/Topological Sort/Topological Sort using BFS.cpp
@@ -5,10 +5,12 @@ Space Complexity: O(N) + O(N) ~ O(2N), O(N) for the indegree array, and O(N) for
 in BFS(where N = no.of nodes)
 
 Algorithm : 
-1. take a array of indegree intially assigned 0 
-2. now put indegree of nodes in array 
-3. take a queue insert node with indegree 0 into the queue and explore its adjacent nodes
-4. while taking nodes from indegree array and pushing into q decrease indegree of node.
+1. Take indegree of all nodes.
+2. push nodes in Queue with indegree 0
+3. pop node from Queue store node in vector ans 
+and reduce the indegree of node 
+    if indegree == 0 then push into Queue 
+
 
 class Solution
 {
@@ -16,39 +18,36 @@ class Solution
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    int indegree[V]={0};
-	   
-	  for(int i=0;i<V;i++)
-	  {
-	   for(auto it : adj[i])
-	   {
-	       indegree[it]++;
-	   }
-	  }
-	
-	queue<int> q;
-	for(int i=0;i<V;i++)
-	{
-	    if(indegree[i]==0){
-	        q.push(i);
+	 int indegree[V]={0};
+	 for(int i=0;i<V;i++)
+	 {
+	    for(auto it:adj[i])
+	    {
+	        indegree[it]++;
 	    }
-	}
-	vector<int> topo;
-	while(!q.empty())
-	{
-	    int node = q.front();
-	    q.pop();
-	    topo.push_back(node);
-	    
-	   for(auto it : adj[node])
-	   {
-	       indegree[it]--;
-	       
-	   if(indegree[it]==0){
-	       q.push(it);
-	   }
-	   }
-	}
-	return topo;
-	}
+	 }
+	 queue<int> q;
+	 
+	 for(int i=0;i<V;i++)
+{
+    if(indegree[i]==0)
+    {
+        q.push(i);
+    }
+}
+vector<int> ans;
+while(!q.empty())
+{
+    int node = q.front();
+    q.pop();
+    ans.push_back(node);
+    for(auto it:adj[node])
+    {
+        indegree[it]--;
+        if(indegree[it]==0)
+        q.push(it);
+    }
+}
+return ans;
+}
 };
